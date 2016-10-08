d522ca8cf29a336959b1c21b11ff519a8a85bcf7
Lukas Christensen <masterzuz@gmail.com>
2016-10-09 00:53:50 +0200

    Repetition

--- scripts/chirp.js
+++ scripts/chirp.js
@@ -5,23 +5,11 @@ var out = context.destination;
 var chirpWaveform = [];
 var arrayBuffer;
 
-var sendChirp = function(){
-    var channels = 1;
-    var frameCount = context.sampleRate * document.getElementById("chirpLength").value;
-    var arrayBuffer = context.createBuffer(channels, frameCount, context.sampleRate);
-
-    for (var channel = 0; channel < channels; channel++) {
-        var nowBuffering = arrayBuffer.getChannelData(channel);
-        var t = 0;
-        for (var i = 0; i < frameCount; i++) {
-            nowBuffering[i] = chirpWaveform[i];
-        }
-    }
-
+var sendChirp = function(){ 
     var source = context.createBufferSource();
     source.buffer = arrayBuffer;
     source.connect(context.destination);
-  
+
     source.start();
 };
 
@@ -31,4 +19,18 @@ var updateChirp = function(){
         t = i / context.sampleRate;
         chirpWaveform[i] = Math.cos(2*Math.PI*document.getElementById("fc").value*t + 2 * Math.PI * document.getElementById("bw").value / document.getElementById("chirpLength").value * (0.5 * t * t - document.getElementById("chirpLength").value / 2 * t));
     }   
+
+    var channels = 1;
+    var frameCount = context.sampleRate * document.getElementById("chirpLength").value;
+    arrayBuffer = context.createBuffer(channels, frameCount, context.sampleRate);
+
+    for (var channel = 0; channel < channels; channel++) {
+        var nowBuffering = arrayBuffer.getChannelData(channel);
+        var t = 0;
+        for (var i = 0; i < frameCount; i++) {
+            nowBuffering[i] = chirpWaveform[i];
+        }
+    }
 };
+
+updateChirp();
--- scripts/sonar.js
+++ scripts/sonar.js
@@ -1,99 +1,111 @@
-function doSonar(button) {
-    sendChirp();
+var functionID;
+
+function startSonar(){
+    doSonar();
+
+    updateChirp();
+    functionID = setInterval(function() {
+        doSonar();
+    }, 2000);
+}
+
+function stopSonar(){
+    window.clearInterval(functionID);
+}
+
+
+
+function doSonar() {
+    sendChirp();        
     //Start recording
-    recorder && recorder.record();
 
-    button.disabled = true;
-    button.nextElementSibling.disabled = false;
-    __log('Recording...');
-    
-    //Stop recording
-    var rawData = [];
     setTimeout(function(){
-        mod = modGain = osc = null;
+        recorder && recorder.record();
 
-        recorder && recorder.stop();
-        button.disabled = false;
-        button.previousElementSibling.disabled = false;
-        __log('Stopped recording.');
+        //Stop recording
+        var rawData = [];
+        setTimeout(function(){
+            mod = modGain = osc = null;
 
+            recorder && recorder.stop();
 
-        // create WAV download link using audio data blob
-        recorder.getBuffer(function(data) {
-            for (var i = 0 ; i < data[0].length ; i++)
-            {
-                rawData[i] = data[0][i];
-            }
-            
-            var maxdelay = chirpWaveform.length;
-
-            var xcorr = [];      
-
-            for (i = 0, n = rawData.length; i < n; i++) {
-                sxy = 0;
-                for (delay = 0; delay < chirpWaveform.length; delay++) {
-                    j = i - delay;
-                    if (j < 0 || j >= n)
-                        continue;
-                    else
-                        sxy += chirpWaveform[delay] * rawData[j];
+            // create WAV download link using audio data blob
+            recorder.getBuffer(function(data) {
+                for (var i = 0 ; i < data[0].length ; i++)
+                {
+                    rawData[i] = data[0][i];
                 }
                 
-                xcorr.push((Math.abs(sxy)));
-            }
+                var maxdelay = chirpWaveform.length;
 
-            var index = 0;
-            for (var i = 0 ; i < xcorr.length ; i++){
-                if (xcorr[i] > 1){
+                var xcorr = [];      
 
-                    index = i;
-                    break;
+                for (i = 0, n = rawData.length; i < n; i++) {
+                    sxy = 0;
+                    for (delay = 0; delay < chirpWaveform.length; delay++) {
+                        j = i - delay;
+                        if (j < 0 || j >= n)
+                            continue;
+                        else
+                            sxy += chirpWaveform[delay] * rawData[j];
+                    }
+                    
+                    xcorr.push((Math.abs(sxy)));
                 }
-            }
+
+                var index = 0;
+                for (var i = 0 ; i < xcorr.length ; i++){
+                    if (xcorr[i] > 1){
+
+                        index = i;
+                        break;
+                    }
+                }
 
 
-            var cp = {
-                x: Array.apply(null, Array(chirpWaveform.length)).map(function (_, i) {return (i-1);}),
-                y: chirpWaveform, 
-                name: 'Chirp',
-                type: 'scatter'
-            };
+                var cp = {
+                    x: Array.apply(null, Array(chirpWaveform.length)).map(function (_, i) {return (i-1);}),
+                    y: chirpWaveform, 
+                    name: 'Chirp',
+                    type: 'scatter'
+                };
 
-            var rec = {
-                x: Array.apply(null, Array(rawData.length)).map(function (_, i) {return (i-1);}),
-                y: rawData, 
-                name: 'Record',
-                type: 'scatter'
-            };
+                var rec = {
+                    x: Array.apply(null, Array(rawData.length)).map(function (_, i) {return (i-1);}),
+                    y: rawData, 
+                    name: 'Record',
+                    type: 'scatter'
+                };
 
-            
-            var xc = {
-                x: Array.apply(null, Array(xcorr.length)).map(function (_, i) {return (i - index) / context.sampleRate * 340.29 / 2;}),
-                y: xcorr, 
-                type: 'scatter'
-            };
+                
+                var xc = {
+                    x: Array.apply(null, Array(xcorr.length)).map(function (_, i) {return (i - index) / context.sampleRate * 340.29 / 2;}),
+                    y: xcorr, 
+                    type: 'scatter'
+                };
 
-            var xcLayout = {
-              xaxis: {title: 'Distance [m]', range: [-.5, 6]},
-              yaxis: {title: 'Reflection Coefficient'},
-              title: 'Sonar Reflection',
-              hovermode: 'closest'
-          };
+                var xcLayout = {
+                  xaxis: {title: 'Distance [m]', range: [-.5, 6]},
+                  yaxis: {title: 'Reflection Coefficient'},
+                  title: 'Sonar Reflection',
+                  hovermode: 'closest'
+              };
 
-          var genLayout = {
-              xaxis: {title: 'Sample #'},
-              yaxis: {title: 'Amplitude'},
-              title: 'Signals',
-              hovermode: 'closest'
-          };
+              var genLayout = {
+                  xaxis: {title: 'Sample #'},
+                  yaxis: {title: 'Amplitude'},
+                  title: 'Signals',
+                  hovermode: 'closest'
+              };
 
-          Plotly.newPlot('myDiv2', [cp, rec],genLayout); 
-          Plotly.newPlot('myDiv', [xc], xcLayout); 
-      });
+              Plotly.newPlot('myDiv2', [cp, rec],genLayout); 
+              Plotly.newPlot('myDiv', [xc], xcLayout); 
+          });
 
-        recorder.clear();
+            recorder.clear();
 
-    }, 1000 * document.getElementById("recTime").value);
+        }, 1000 * document.getElementById("recTime").value);
+    }, 500);
 }
 
 function __log(e, data) {
