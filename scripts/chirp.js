1e34a1739fa7e48180f047b6f17038d7a91ec35c
Lukas Christensen <masterzuz@gmail.com>
2016-10-08 05:03:48 +0200

    Finally found out how to plot things.

--- scripts/chirp.js
+++ scripts/chirp.js
@@ -2,7 +2,19 @@ var context = new (window.AudioContext || window.webkitAudioContext)();
 var mod, modGain, osc;
 var out = context.destination;
 
+var drawChirp = function(){
+
+    var chirp = {
+      x: chirpX, 
+      y: chirpY, 
+      type: 'scatter'
+    };
+
+    Plotly.newPlot('myDiv', [chirp]);
+};
+
 var startTest = function(){
+
     mod = context.createOscillator();
     mod.type = 'sawtooth';
     mod.frequency.value = .5 * 100;
@@ -23,7 +35,6 @@ var startTest = function(){
     var stopTime = context.currentTime + 0.5 / mod.frequency.value ;
     osc.stop(stopTime);
     mod.stop(stopTime);
-    modGain.stop(stopTime);
 
     setTimeout(function(){
     		mod = modGain = osc = null;
@@ -33,3 +44,5 @@ var startTest = function(){
 document.getElementById("start").onclick = function(){
   startTest();
 };
+
+drawChirp();
