020fe4701683a8309980515450c741232d2d992e
Lukas Christensen <masterzuz@gmail.com>
2016-10-08 12:58:54 +0200

    Merged!

--- scripts/chirp.js
+++ scripts/chirp.js
@@ -3,10 +3,19 @@ var mod, modGain, osc;
 var out = context.destination;
 
 var drawChirp = function(){
+    var fs = 44100;
+    var N = document.getElementById("chirpLength").value * fs;
+
+    var time = Array.apply(null, Array(N)).map(function (_, i) {return (i-1) / fs;});
+    var signal = [];
+
+    for (var i = 0; i < N; i++) {
+        signal[i] = Math.cos(2*Math.PI*document.getElementById("fc").value*time[i] + 2 * Math.PI * document.getElementById("bw").value / document.getElementById("chirpLength").value * (0.5 * time[i] * time[i] - document.getElementById("chirpLength").value / 2 * time[i]));;
+    }
 
     var chirp = {
-      x: chirpX, 
-      y: chirpY, 
+      x: time, 
+      y: signal, 
       type: 'scatter'
     };
 
@@ -17,19 +26,19 @@ var startTest = function(){
 
     mod = context.createOscillator();
     mod.type = 'sawtooth';
-    mod.frequency.value = .5 * 100;
+    mod.frequency.value = .5 * 1/document.getElementById("chirpLength").value;
     
     modGain = context.createGain();
-    modGain.gain.value = 2000;
+    modGain.gain.value = document.getElementById("bw").value;
     
     osc = context.createOscillator();
-    osc.frequency.value = 20000;
+    osc.frequency.value = document.getElementById("fc").value;
     
     mod.connect(modGain);
     modGain.connect(osc.frequency);
     osc.connect(out);
 
-	osc.start();
+    osc.start();
     mod.start();
     
     var stopTime = context.currentTime + 0.5 / mod.frequency.value ;
@@ -37,12 +46,13 @@ var startTest = function(){
     mod.stop(stopTime);
 
     setTimeout(function(){
-    		mod = modGain = osc = null;
+            mod = modGain = osc = null;
     }, 500 / mod.frequency.value);
 };
 
 document.getElementById("start").onclick = function(){
   startTest();
+  drawChirp();
 };
 
 drawChirp();
